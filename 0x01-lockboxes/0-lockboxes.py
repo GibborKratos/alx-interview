2d47f8b7df5e39d5591285cf5df881cb42501b46
GibborKratos <gibbsonanita@gmail.com>
2024-05-02 11:27:06 +0000

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,20 +2,24 @@
 '''0x01. Lockboxes
 '''
 
+
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
-
+    '''You have n number of locked boxes in front of you.
+      Each box is numbered sequentially from 0 to n - 1
+        and each box may contain keys to the other boxes,
+        this function determines if all the boxes can be opened.
+    '''
     n = len(boxes)
-    keys = set([0])  # Initialize with the key to the first box
-    visited = set()
 
-    while keys:
-        box = keys.pop()
-        visited.add(box)
-        for key in boxes[box]:
-            if key < n and key not in visited:
-                keys.add(key)
+    opened_boxes = set()
+    opened_boxes.add(0)
 
-    return len(visited) == n
-
+    to_explore = set(boxes[0]).difference(set([0]))
+    while len(to_explore) > 0:
+        current_box = to_explore.pop()
+        if not current_box or current_box >= n or current_box < 0:
+            continue
+        if current_box not in opened_boxes:
+            to_explore = to_explore.union(boxes[current_box])
+            opened_boxes.add(current_box)
+    return n == len(opened_boxes)
