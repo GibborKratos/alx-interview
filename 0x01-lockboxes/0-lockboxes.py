b4a64f56f9ad287f8631a64098adc293e8249500
GibborKratos <gibbsonanita@gmail.com>
2024-07-29 16:25:30 +0000

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
-"""Lockboxes module.
-
-This module contains a function related to the lockboxes.
-"""
-
 
 def canUnlockAll(boxes):
-    """Determines if all the boxes can be opened.
-
-    Args:
-        boxes (lst): the list of boxes containing a list of keys.
-
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
     """
-    box_count = len(boxes)
+	take boxes
+		create set of keys
+			go to box
+			get all keys and add them setpfkeys
+start opening boxes from setofkeys
+go to each box of each key
+		and take the keys from it and add them to set of keys
+		keep looping through all setof key
+			ignore keys that dont have box
+		track opening of boxes by a counter, if at the en fit equals to length
+		 of boxes it means all boxes unlock
+  """
+   length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    if box_count > 0:
-        keys = boxes[0]
-        boxes[0] = [-1]
-        while keys:
-            new_keys = []
-            for key in keys:
-                if key < box_count:
-                    if boxes[key] != [-1]:
-                        new_keys += boxes[key]
-                        boxes[key] = [-1]
-            keys = new_keys
+    while i < length:
+        oldo = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldo != i:
+            continue
+        else:
+            break
 
-        return boxes.count([-1]) == box_count
-
-    return False
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
